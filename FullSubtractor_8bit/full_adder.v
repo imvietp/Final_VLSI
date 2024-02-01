672f18016883a8121aec1065e2a856987651389a
Phạm Việt <125435826+imvietp@users.noreply.github.com>
2024-02-01 19:03:49 +0700

    Add files via upload

---
+++ FullSubtractor_8bit/full_adder.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:48:56 02/01/2024 
+// Design Name: 
+// Module Name:    full_adder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module full_adder(
+    output cout,
+    output sum,
+    input ain,
+    input bin,
+    input cin
+    );
+
+    //assign bin2 = cin^bin; // XORing the inputs to bin with cin. If 1, subtract; if 0, add
+    
+    assign sum = ain^bin^cin;
+    assign cout = (ain&bin) | (ain&cin) | (bin&cin);
+endmodule
---
+++ FullSubtractor_8bit/testbench.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   18:53:21 02/01/2024
+// Design Name:   adder8
+// Module Name:   E:/Tai lieu dai hoc/HK2 23 - 24/Vi_mach/verilog/FullSubtractor_8bit/testbench.v
+// Project Name:  FullSubtractor_8bit
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: adder8
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module testbench;
+
+	// Inputs
+	reg [7:0] a;
+	reg [7:0] b;
+	reg cin;
+
+	// Outputs
+	wire cout;
+	wire [7:0] s;
+
+	// Instantiate the Unit Under Test (UUT)
+	adder8 uut (
+		.cout(cout), 
+		.s(s), 
+		.a(a), 
+		.b(b), 
+		.cin(cin)
+	);
+
+	initial begin
+		// Initialize Inputs
+	// Initialize Inputs
+		a = 8'b00011100;
+		b = 8'b11110000;
+		cin = 1;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		a = 8'b00011101;
+		b = 8'b11110010;
+		cin = 1;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		
+		a = 8'b10011100;
+		b = 8'b11111100;
+		cin = 1;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
